72df236e80b9d3208e72da214d7a5a7307b81153
whqtker <chosh2001@inha.edu>
2026-05-31 19:30:25 +0900

    test: expand TrackParams assertions to all 4 tracks and key fields

--- Bot_Java/TestRunner.java
+++ Bot_Java/TestRunner.java
@@ -6,15 +6,32 @@ public class TestRunner {
 
     static void testTrackParams() {
         MyCar.TrackParams basic = MyCar.PARAMS[MyCar.TRACK_BASIC];
-        assertTrue(basic.maxSpeed == 130f, "BASIC maxSpeed should be 130");
-        assertTrue(basic.minSpeed == 40f,  "BASIC minSpeed should be 40");
-        assertTrue(basic.steerLookAhead == 5, "BASIC steerLookAhead should be 5");
-        assertTrue(basic.speedLookAhead == 6, "BASIC speedLookAhead should be 6");
+        assertTrue(basic.maxSpeed == 130f,       "BASIC maxSpeed");
+        assertTrue(basic.minSpeed == 40f,        "BASIC minSpeed");
+        assertTrue(basic.slowdownFactor == 0.8f, "BASIC slowdownFactor");
+        assertTrue(basic.steerLookAhead == 5,    "BASIC steerLookAhead");
+        assertTrue(basic.speedLookAhead == 6,    "BASIC speedLookAhead");
+        assertTrue(basic.K1 == 0.3f,            "BASIC K1");
+        assertTrue(basic.brakeRange == 40f,      "BASIC brakeRange");
+
+        MyCar.TrackParams speed = MyCar.PARAMS[MyCar.TRACK_SPEED];
+        assertTrue(speed.maxSpeed == 120f,       "SPEED maxSpeed");
+        assertTrue(speed.slowdownFactor == 0.9f, "SPEED slowdownFactor");
+        assertTrue(speed.steerLookAhead == 6,    "SPEED steerLookAhead");
+        assertTrue(speed.K2 == 0.4f,            "SPEED K2");
+
+        MyCar.TrackParams ssafy = MyCar.PARAMS[MyCar.TRACK_SSAFY];
+        assertTrue(ssafy.maxSpeed == 110f,       "SSAFY maxSpeed");
+        assertTrue(ssafy.slowdownFactor == 1.0f, "SSAFY slowdownFactor");
+        assertTrue(ssafy.decayFactor == 0.3f,   "SSAFY decayFactor");
+        assertTrue(ssafy.accelerationRange == 25f, "SSAFY accelerationRange");
 
         MyCar.TrackParams germany = MyCar.PARAMS[MyCar.TRACK_GERMANY];
-        assertTrue(germany.maxSpeed == 100f, "GERMANY maxSpeed should be 100");
-        assertTrue(germany.steerLookAhead == 8, "GERMANY steerLookAhead should be 8");
-        assertTrue(germany.speedLookAhead == 10, "GERMANY speedLookAhead should be 10");
+        assertTrue(germany.maxSpeed == 100f,       "GERMANY maxSpeed");
+        assertTrue(germany.steerLookAhead == 8,    "GERMANY steerLookAhead");
+        assertTrue(germany.speedLookAhead == 10,   "GERMANY speedLookAhead");
+        assertTrue(germany.K3 == 0.4f,            "GERMANY K3");
+        assertTrue(germany.brakeRange == 30f,      "GERMANY brakeRange");
 
         System.out.println("PASS: TrackParams");
     }
